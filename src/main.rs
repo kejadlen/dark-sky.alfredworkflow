f9718bbd84b95bd0f5c6d7060161f8669cd105f9
Alpha Chen <alpha.chen@gmail.com>
2017-10-12 19:50:32 -0700

    Implement coordinate deserialization

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,16 @@
 #[macro_use]
 extern crate error_chain;
 extern crate reqwest;
+extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
 
 mod errors;
+
+use std::result;
+
+use serde::Deserialize;
 
 use errors::*;
 
@@ -21,9 +26,35 @@ fn run() -> Result<()> {
     Ok(())
 }
 
+#[derive(Debug)]
+struct Coordinate {
+    lat: f64,
+    long: f64,
+}
+
 #[derive(Debug, Deserialize)]
 struct IPInfo {
-    loc: String,
+    #[serde(rename = "loc", deserialize_with = "coordinate_from_string")]
+    location: Coordinate,
     city: String,
     region: String,
+}
+
+fn coordinate_from_string<'de, D>(deserializer: D) -> result::Result<Coordinate, D::Error>
+where
+    D: ::serde::Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    let mut split = s.split(',');
+    let lat = split.next().and_then(|lat| lat.parse().ok()).ok_or_else(
+        || {
+            ::serde::de::Error::custom("")
+        },
+    )?;
+    let long = split.next().and_then(|long| long.parse().ok()).ok_or_else(
+        || {
+            ::serde::de::Error::custom("")
+        },
+    )?;
+    Ok(Coordinate { lat, long })
 }
